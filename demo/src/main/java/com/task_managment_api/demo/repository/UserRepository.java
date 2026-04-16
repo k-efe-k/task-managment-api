a26fe63d1a40ef5304acc2ea96ce94fa77999e31
KADİR EFE KAYHAN <kadirkayhan@stu.aydin.edu.tr>
2026-04-16 21:36:07 +0300

    security layers has been ready

--- demo/src/main/java/com/task_managment_api/demo/repository/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.task_managment_api.demo.repository;
-
-public class UserRepository {
-}
--- demo/src/main/java/com/task_managment_api/demo/security/JwtService.java
+++ demo/src/main/java/com/task_managment_api/demo/security/JwtService.java
@@ -1,4 +1,56 @@
 package com.task_managment_api.demo.security;
 
+import com.task_managment_api.demo.domain.entity.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private Long expiration;
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .claim("role", user.getRole().name())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, User user) {
+        final String email = extractEmail(token);
+        return email.equals(user.getEmail()) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    private Claims extractClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
 }
